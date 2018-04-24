9bf7d98843a52e877888be76140cc73601411043
Jo-Creep <Jo-Creep@users.noreply.github.com>
2018-04-24 09:01:30 +0300

    for loop

---
+++ JS/for-loop.js
@@ -0,0 +1,12 @@
+for (i = 1; i < 11; i++)
+{
+    document.write(i +" ");
+}
+
+/* for loop syntax:
+
+for(initialization; condition; modification)
+{
+    // code goes here
+    // do something
+} */
